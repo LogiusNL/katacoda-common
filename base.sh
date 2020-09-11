2d3a166ed84aadba1b38673a12861a2a0bd540da
Jeffrey den Drijver <jeedendree@gmail.com>
2020-09-11 20:01:18 +0200

    Initial attempt at moving logging to seperate file

--- base.sh
+++ base.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
 
 ### UTILITIES ###
-
-# Set Colors
-bold=$(tput smso)
-underline=$(tput sgr 0 1)
-reset=$(tput sgr0)
-
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-white=$(tput setaf 0)
-tan=$(tput setaf 3)
-blue=$(tput setaf 4)
-
-# Headers and Logging
-underline() { printf "${underline}${bold}%s${reset}\n" "$@"
-}
-h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@"
-}
-h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@"
-}
-debug() { printf "${white}%s${reset}\n" "$@"
-}
-info() { printf "${white}➜ %s${reset}\n" "$@"
-}
-success() { printf "${green}✔ %s${reset}\n" "$@"
-}
-error() { printf "${red}✖ %s${reset}\n" "$@"
-}
-warn() { printf "${tan}➜ %s${reset}\n" "$@"
-}
-bold() { printf "${bold}%s${reset}\n" "$@"
-}
-note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@"
-}
+. logging.sh
 
 function get_helm3 {
   warn "Updating to helm3"
---
+++ logging.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set Colors
+bold=$(tput smso)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+white=$(tput setaf 0)
+tan=$(tput setaf 3)
+blue=$(tput setaf 4)
+
+# Headers and Logging
+underline() { printf "${underline}${bold}%s${reset}\n" "$@"
+}
+h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@"
+}
+h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@"
+}
+debug() { printf "${white}%s${reset}\n" "$@"
+}
+info() { printf "${white}➜ %s${reset}\n" "$@"
+}
+success() { printf "${green}✔ %s${reset}\n" "$@"
+}
+error() { printf "${red}✖ %s${reset}\n" "$@"
+}
+warn() { printf "${tan}➜ %s${reset}\n" "$@"
+}
+bold() { printf "${bold}%s${reset}\n" "$@"
+}
+note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@"
+}
