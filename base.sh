0b2c53d8c8750914ae571d7d92ce096802f65420
Jeffrey den Drijver <JeffreyDD@users.noreply.github.com>
2020-12-07 12:59:53 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -13,6 +13,27 @@ function get_ansible {
   ansible-galaxy collection install community.kubernetes
 }
 
+function deploy_longhorn {
+  warn "Installing open-iscsi on nodes"
+  apt install -y open-iscsi
+  ssh node01 apt install -y open-iscsi
+  warn "Deploying Longhorn Storage Provider"
+  kubectl create namespace longhorn-system
+  helm repo add longhorn https://charts.longhorn.io
+  helm install longhorn longhorn/longhorn \
+    --namespace longhorn-system \
+    --set persistence.defaultClass=true \
+    --set persistence.defaultClassReplicaCount=1 \
+    --set persistence.reclaimPolicy=Delete \
+    --set defaultSettings.guaranteedEngineCPU=0.1 \
+    --set defaultSettings.taintToleration="stc.subku.be/storage=true:PreferNoSchedule;" \
+    --set enablePSP=true \
+    --set resources.limits.cpu=50m \
+    --set resources.limits.memory=128Mi \
+    --set resources.requests.cpu=50m \
+    --set resources.requests.memory=128Mi
+}
+
 function deploy_nginx {
   warn "Deploying Nginx Ingress Controller"
   helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
